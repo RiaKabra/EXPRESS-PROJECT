756f8faf9d3d4fc313684d6d3e65f4d75cf986c5
RiaKabra <riakabra1@gmail.com>
2024-07-20 11:47:15 +0530

    [Ria]Updated: added getAll

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,23 +1,28 @@
 import Note from '../models/note.model';
 
+export const getAll = async (value) => {
+  const data = await Note.find({createdBy:value});
+  return data;
+};
+
 export const createNote = async (noteDetails) => {
   const data = await Note.create(noteDetails);
   return data;
 };
 
-export const retrieveNote = async (noteDetails) => {
-    const data = await Note.findById(noteDetails);
+export const retrieveNote = async (noteCreatedBy,noteId) => {
+    const data = await Note.findOne({createdBy:noteCreatedBy,_id:noteId});
     return data;
   };
 
-export const updateNote = async (noteDetails) => {
-    const data = await Note.updateOne(noteDetails);
+export const updateNote = async (value,id) => {
+    const data = await Note.findOneAndUpdate({createdBy:value,_id : id});
     return data;
   };
 
 
-export const deleteNote = async (noteid) => {
+export const deleteNote = async (created,noteid) => {
     console.log(noteid);
-    const data = await Note.findByIdAndDelete({_id:noteid});
+    const data = await Note.findOneAndDelete({createdBy:created,_id:noteid});
     return data;
   };
