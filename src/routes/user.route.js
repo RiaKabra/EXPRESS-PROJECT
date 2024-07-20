a71951d4dff23f6984c6875fc60a02ce4181d4e8
RiaKabra <riakabra1@gmail.com>
2024-07-20 11:46:49 +0530

    [Ria]Updated:changed endpoints

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,8 +3,8 @@ import { newUserValidator } from '../validators/user.validator.js';
 import * as userController from '../controllers/user.controller.js';
 const router = express.Router();
 
-router.get('/get', userController.loginUser);
-router.post('/new', userController.createUser);
+router.get('/login', userController.loginUser);
+router.post('/signup', userController.createUser);
 
 
 export default router;
