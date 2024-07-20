1b0401591a506e3a51fb72412b0b799f8b1f6c7a
RiaKabra <riakabra1@gmail.com>
2024-07-20 11:46:34 +0530

    [Ria]Updated:Added getAll route

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import * as noteController from '../controllers/note.controller.js';
+import { noteValidator } from '../validators/note.validator.js';
 import {userAuth} from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
-router.post('/create',userAuth, noteController.createNote);
+router.get('',userAuth,noteController.getAll);
+router.post('', noteValidator, userAuth, noteController.createNote);
 router.get('/:_id',userAuth,noteController.retrieveNote);
-router.put('/:_id',userAuth, noteController.updateNote);
+router.put('/:_id',noteValidator, userAuth, noteController.updateNote);
 router.delete('/:_id',userAuth, noteController.deleteNote);
 
 export default router;
