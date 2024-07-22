857eddeeaf3ea5a0dd7833ab3af87f707653e5de
RiaKabra <riakabra1@gmail.com>
2024-07-22 22:08:50 +0530

    [Ria]Updated:routes for archive and trash

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -6,14 +6,12 @@ const router = express.Router();
 
 router.get('',userAuth,noteController.getAll);
 router.post('', noteValidator, userAuth, noteController.createNote);
-router.post('/archived/:_id', userAuth, noteController.toggleArchiveNote)
-router.post('/unarchived/:_id', userAuth, noteController.toggleArchiveNote);
+router.post('/is_arch_unarch/:_id', userAuth, noteController.toggleArchiveNote)
 router.get('/:_id',userAuth,noteController.retrieveNote);
 router.put('/:_id',noteValidator, userAuth, noteController.updateNote);
 router.delete('/:_id',userAuth, noteController.deleteNote);
 router.patch('/colour/:_id', userAuth, noteController.colourUpdate);
-router.delete('/trash/:_id', userAuth, noteController.toggleTrashNote);
-router.post('/untrash/:_id', userAuth, noteController.toggleTrashNote);
+router.post('/is_trash_untrash/:_id', userAuth, noteController.toggleTrashNote);
 export default router;
 
 
