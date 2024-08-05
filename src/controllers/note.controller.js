9c310ce06ce08045697be8ca2120256dd062e381
RiaKabra <riakabra1@gmail.com>
2024-08-05 16:08:24 +0530

    [Ria]Modified: getAll using redis cache

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,21 +1,21 @@
 import * as noteService from "../services/note.service";
 import HttpStatus from "http-status-codes";
+
 export const getAll = async (req, res, next) => {
     try {
-        const getAll = await noteService.getAll(req.body.createdBy);
-        res.status(HttpStatus.OK).json({
-            code: HttpStatus.OK,
-            note: getAll,
-            message: "All notes retrieved"
-        })
+      const getAllNotes = await noteService.getAll(req.body.createdBy);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        note: getAllNotes,
+        message: "All notes retrieved"
+      });
     } catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: ` ${error}`
-        })
-
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        message: ` ${error}`
+      });
     }
-};
+  };
 export const createNote = async (req, res, next) => {
     try {
         console.log(req.body);
@@ -24,6 +24,7 @@ export const createNote = async (req, res, next) => {
             code: HttpStatus.CREATED,
             note: note,
             message: "Note created successfully"
+            
         });
     } catch (error) {
         res.status(HttpStatus.BAD_REQUEST).json({
@@ -37,6 +38,13 @@ export const createNote = async (req, res, next) => {
 export const retrieveNote = async (req, res, next) => {
     try {
         const retrieveNote = await noteService.retrieveNote(req.body.createdBy, req.params._id);
+        if(!retrieveNote)
+        {
+            return res.status(HttpStatus.BAD_REQUEST).json({
+                code:HttpStatus.BAD_REQUEST,
+                message:"Note not found"
+            })
+        }
         res.status(HttpStatus.OK).json({
             code: HttpStatus.OK,
             note: retrieveNote,
@@ -53,6 +61,7 @@ export const retrieveNote = async (req, res, next) => {
 
 export const updateNote = async (req, res, next) => {
     try {
+        console.log("Request body and paramters---->",req.body,req.params);
         const updatedNote = await noteService.updateNote(req.body, req.params._id);
         res.status(HttpStatus.OK).json({
             code: HttpStatus.OK,
@@ -67,57 +76,34 @@ export const updateNote = async (req, res, next) => {
     }
 };
 
-
-export const deleteNote = async (req, res, next) => {
-    try {
-        const deleteNote = await noteService.deleteNote(req.body.createdBy, req.params._id);
-        res.status(HttpStatus.OK).json({
-            code: HttpStatus.OK,
-            message: "Note deleted successfully"
-        });
-    }
-    catch (error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code: HttpStatus.BAD_REQUEST,
-            message: ` ${error}`
-        });
-    }
+export const toggleArchiveNote = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const note = await noteService.toggleArchiveNote(_id);
+    res.status(HttpStatus.OK).json({
+      message: "Note archive status changed successfully",
+      data: note
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      message: `${error}`
+    });
+  }
 };
 
-
-export const toggleArchiveNote = async(req,res,next) => {
-    try {
-        const archOrUnarch = await noteService.toggleArchiveNote(req.params._id);;
-        res.status(HttpStatus.OK).json({
-            code:HttpStatus.OK,
-            message: "Note archive status changes successfully",
-            data:archOrUnarch
-        });
-    }
-    catch(error) {
-        res.status(HttpStatus.BAD_REQUEST).json({
-           code:HttpStatus.BAD_REQUEST,
-           message : ` ${error}`
-        });
-    }
-};
-
-export const toggleTrashNote = async(req,res,next) => {
-    try{
-        const trashOrUntrash = await noteService.toggleTrashNote(req.params._id);
-        res.status(HttpStatus.OK).json({
-            code:HttpStatus.OK,
-            message:"Note trash status changes succesfully",
-            data: trashOrUntrash
-        });
-    }
-    catch(error)
-    {
-        res.status(HttpStatus.BAD_REQUEST).json({
-            code:HttpStatus.BAD_REQUEST,
-            message: ` ${error}`
-        })
-    }
+export const toggleTrashNote = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const note = await noteService.toggleTrashNote(_id);
+    res.status(HttpStatus.OK).json({
+      message: "Note trash status changed successfully",
+      data: note
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      message: `${error}`
+    });
+  }
 };
 
 export const colourUpdate = async(req,res,next) => {
@@ -135,5 +121,28 @@ export const colourUpdate = async(req,res,next) => {
             code:HttpStatus.BAD_REQUEST,
             message:`$error`
         })
+    }
+};
+
+export const deleteNote = async (req, res, next) => {
+    try {
+        const deleteNote = await noteService.deleteNote(req.body.createdBy, req.params._id);
+        if(!deleteNote)
+        {
+            return res.status(HttpStatus.BAD_REQUEST).json({
+                code:HttpStatus.BAD_REQUEST,
+                message:"Note not found"
+            });
+        }
+        res.status(HttpStatus.OK).json({
+            code: HttpStatus.OK,
+            message: "Note deleted successfully"
+        });
+    }
+    catch (error) {
+        res.status(HttpStatus.BAD_REQUEST).json({
+            code: HttpStatus.BAD_REQUEST,
+            message: ` ${error}`
+        });
     }
 };
